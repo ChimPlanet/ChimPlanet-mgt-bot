42486dd9fdad9a3239518cad1a12411952b83df9
SeonWooJu <tjsdnwn21104@gmail.com>
2023-12-16 03:22:26 +0900

    .properties 읽어 오는 Class 생성

--- src/main/java/org/example/util/PropertiesLoad.java
+++ src/main/java/org/example/util/PropertiesLoad.java
@@ -1,5 +1,20 @@
 package org.example.util;
 
-public class PropertiesLoad {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
 
+public class PropertiesLoad {
+    public String PropertiesLoad(String name) {
+        try {
+            Properties prop = new Properties();
+
+            prop.load(new FileInputStream("./src/main/resources/application.properties"));
+
+            return prop.getProperty(name);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }

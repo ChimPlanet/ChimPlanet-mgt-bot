d8e12a851969ea879581ae3836c312b1f45ffb67
SeonWooJu <tjsdnwn21104@gmail.com>
2023-12-16 02:57:28 +0900

    Apply .gitignore

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Main extends ListenerAdapter {
+
+    public static void main(String[] args){
+        JDA jda = JDABuilder.createDefault("")
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+
+        jda.addEventListener(new Main());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println(event.getMessage().getContentDisplay());
+
+        if (event.getMessage().getContentRaw().equals("!ping")) {
+            event.getChannel().sendMessage("pong!").queue();
+        }
+    }
+}

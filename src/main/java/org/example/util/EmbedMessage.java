2021233717a90aeb161a2fd9b9fd94a0db04707a
SeonWooJu <tjsdnwn21104@gmail.com>
2023-12-16 06:29:53 +0900

    EmbedMessage Class ErrorEmbed 메서드 생성

--- src/main/java/org/example/util/EmbedMessage.java
+++ src/main/java/org/example/util/EmbedMessage.java
@@ -5,13 +5,22 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import java.awt.*;
 
 public class EmbedMessage {
-    public EmbedBuilder getEmbed (String title, String description) {
+    public EmbedBuilder getEmbed (String title, String content) {
         EmbedBuilder eb = new EmbedBuilder();
 
         eb.setTitle(title, null);   // Embed 형식 메시지의 Title
         eb.setColor(Color.green);   // Embed 형식 메시지의 왼쪽 줄 생상
+        eb.setDescription(content);   // String 형식으로 저장된 파일 목록을 출력
 
-        eb.setDescription(description);   // String 형식으로 저장된 파일 목록을 출력
+        return eb;
+    }
+
+    public EmbedBuilder getErorrEmbed (String content) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        eb.setTitle(":octagonal_sign: Error", null);
+        eb.setColor(Color.red);
+        eb.setDescription(content);
 
         return eb;
     }

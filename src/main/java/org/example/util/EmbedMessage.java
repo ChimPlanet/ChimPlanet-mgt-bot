6fd82d33a595823fae72199706944940bc02dd59
SeonWooJu <tjsdnwn21104@gmail.com>
2023-12-16 07:24:01 +0900

    EmbedMessage 주석 오타 수정 생상 -> 색상

--- src/main/java/org/example/util/EmbedMessage.java
+++ src/main/java/org/example/util/EmbedMessage.java
@@ -10,7 +10,7 @@ public class EmbedMessage {
         EmbedBuilder eb = new EmbedBuilder();
 
         eb.setTitle(title, null);   // Embed 형식 메시지의 Title
-        eb.setColor(Color.green);   // Embed 형식 메시지의 왼쪽 줄 생상
+        eb.setColor(Color.green);   // Embed 형식 메시지의 왼쪽 줄 색상
         eb.setDescription(content);   // String 형식으로 저장된 파일 목록을 출력
 
         return eb;
@@ -21,7 +21,7 @@ public class EmbedMessage {
         EmbedBuilder eb = new EmbedBuilder();
 
         eb.setTitle(":octagonal_sign: Error", null);    // Embed 형식 메시지의 Title
-        eb.setColor(Color.red); // Embed 형식 메시지의 왼쪽 줄 생상
+        eb.setColor(Color.red); // Embed 형식 메시지의 왼쪽 줄 색상
         eb.setDescription(content); // String 형식으로 저장된 파일 목록을 출력
 
         return eb;
